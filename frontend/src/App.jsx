6a8e27466a2857d258c496e52df64bbe68bca7f4
Usuário <usuario@MacBook-Air-de-Usuario.local>
2026-01-15 18:26:18 -0300

    Atualização geral e correções

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 // App.jsx
 import { useState } from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { AppBar, Toolbar, Button, Box, Typography, Menu, MenuItem, createTheme, ThemeProvider, CssBaseline } from "@mui/material";
+import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
+import { AppBar, Toolbar, Button, Box, Typography, Menu, MenuItem, createTheme, ThemeProvider, CssBaseline, Dialog, DialogTitle, DialogContent, DialogActions, TextField } from "@mui/material";
 import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
 import Ingredients from "./pages/Ingredients";
@@ -79,6 +79,12 @@ export default function App() {
   const [anchorCons, setAnchorCons] = useState(null);
   const [anchorPed, setAnchorPed] = useState(null);
 
+  // Estados de Autenticação
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [loginOpen, setLoginOpen] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   const openCad = Boolean(anchorCad);
   const openCons = Boolean(anchorCons);
   const openPed = Boolean(anchorPed);
@@ -87,6 +93,22 @@ export default function App() {
     setAnchorCad(null);
     setAnchorCons(null);
     setAnchorPed(null);
+  };
+
+  const handleLogin = () => {
+    if (username === "tkookies_" && password === "TKookies") {
+      setIsLoggedIn(true);
+      setLoginOpen(false);
+      setUsername("");
+      setPassword("");
+    } else {
+      alert("Usuário ou senha incorretos!");
+    }
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    handleClose();
   };
 
   return (
@@ -100,53 +122,97 @@ export default function App() {
           </Typography>
           <Box display="flex" gap={1}>
             <Button color="inherit" component={Link} to="/">HOME</Button>
-            {/* Menu Cadastros */}
-            <Button color="inherit" onClick={(e) => setAnchorCad(e.currentTarget)}>CADASTROS</Button>
-            <Menu anchorEl={anchorCad} open={openCad} onClose={handleClose}>
-              <MenuItem component={Link} to="/produtos/novo" onClick={handleClose}>Novo Produto</MenuItem>
-              <MenuItem component={Link} to="/ingredientes/novo" onClick={handleClose}>Novo Ingrediente</MenuItem>
-              <MenuItem component={Link} to="/clientes/novo" onClick={handleClose}>Novo Cliente</MenuItem>
-              <MenuItem component={Link} to="/combos/novo" onClick={handleClose}>Novo Combo</MenuItem>
-            </Menu>
+            
+            {isLoggedIn ? (
+              <>
+                {/* Menu Cadastros */}
+                <Button color="inherit" onClick={(e) => setAnchorCad(e.currentTarget)}>CADASTROS</Button>
+                <Menu anchorEl={anchorCad} open={openCad} onClose={handleClose}>
+                  <MenuItem component={Link} to="/produtos/novo" onClick={handleClose}>Novo Produto</MenuItem>
+                  <MenuItem component={Link} to="/ingredientes/novo" onClick={handleClose}>Novo Ingrediente</MenuItem>
+                  <MenuItem component={Link} to="/clientes/novo" onClick={handleClose}>Novo Cliente</MenuItem>
+                  <MenuItem component={Link} to="/combos/novo" onClick={handleClose}>Novo Combo</MenuItem>
+                </Menu>
 
-            {/* Menu Consultas */}
-            <Button color="inherit" onClick={(e) => setAnchorCons(e.currentTarget)}>CONSULTAS</Button>
-            <Menu anchorEl={anchorCons} open={openCons} onClose={handleClose}>
-              <MenuItem component={Link} to="/produtos" onClick={handleClose}>Produtos</MenuItem>
-              <MenuItem component={Link} to="/ingredientes" onClick={handleClose}>Ingredientes</MenuItem>
-              <MenuItem component={Link} to="/clientes" onClick={handleClose}>Clientes</MenuItem>
-              <MenuItem component={Link} to="/combos" onClick={handleClose}>Combos</MenuItem>
-              <MenuItem component={Link} to="/estoque" onClick={handleClose}>Estoque</MenuItem>
-            </Menu>
+                {/* Menu Consultas */}
+                <Button color="inherit" onClick={(e) => setAnchorCons(e.currentTarget)}>CONSULTAS</Button>
+                <Menu anchorEl={anchorCons} open={openCons} onClose={handleClose}>
+                  <MenuItem component={Link} to="/produtos" onClick={handleClose}>Produtos</MenuItem>
+                  <MenuItem component={Link} to="/ingredientes" onClick={handleClose}>Ingredientes</MenuItem>
+                  <MenuItem component={Link} to="/clientes" onClick={handleClose}>Clientes</MenuItem>
+                  <MenuItem component={Link} to="/combos" onClick={handleClose}>Combos</MenuItem>
+                  <MenuItem component={Link} to="/estoque" onClick={handleClose}>Estoque</MenuItem>
+                </Menu>
 
-            {/* Menu Pedidos */}
-            <Button color="inherit" onClick={(e) => setAnchorPed(e.currentTarget)}>PEDIDOS</Button>
-            <Menu anchorEl={anchorPed} open={openPed} onClose={handleClose}>
-              <MenuItem component={Link} to="/pedidos/novo" onClick={handleClose}>Novo Pedido</MenuItem>
-              <MenuItem component={Link} to="/pedidos" onClick={handleClose}>Consultar Pedidos</MenuItem>
-            </Menu>
+                {/* Menu Pedidos */}
+                <Button color="inherit" onClick={(e) => setAnchorPed(e.currentTarget)}>PEDIDOS</Button>
+                <Menu anchorEl={anchorPed} open={openPed} onClose={handleClose}>
+                  <MenuItem component={Link} to="/pedidos/novo" onClick={handleClose}>Novo Pedido</MenuItem>
+                  <MenuItem component={Link} to="/pedidos" onClick={handleClose}>Consultar Pedidos</MenuItem>
+                </Menu>
+                <Button color="inherit" onClick={handleLogout}>SAIR</Button>
+              </>
+            ) : (
+              <Button color="inherit" onClick={() => setLoginOpen(true)}>LOGIN</Button>
+            )}
           </Box>
         </Toolbar>
       </AppBar>
       <Box sx={{ py: 4, minHeight: '100vh' }}>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/produtos" element={<Dashboard />} />
-          <Route path="/produtos/novo" element={<Products />} />
-          <Route path="/ingredientes" element={<Ingredients />} />
-          <Route path="/ingredientes/novo" element={<IngredientForm />} />
-          <Route path="/clientes" element={<Clients />} />
-          <Route path="/clientes/novo" element={<ClientForm />} />
-          <Route path="/pedidos" element={<Orders />} />
-          <Route path="/pedidos/novo" element={<OrderForm />} />
-          <Route path="/pedidos/:id" element={<OrderForm />} />
-          <Route path="/combos" element={<Combos />} />
-          <Route path="/combos/novo" element={<ComboForm />} />
-          <Route path="/combos/:id" element={<ComboForm />} />
-          <Route path="/estoque" element={<Inventory />} />
-          <Route path="/production" element={<Production />} />
+          <Route path="/" element={<Home isLoggedIn={isLoggedIn} onLoginClick={() => setLoginOpen(true)} />} />
+          
+          {isLoggedIn ? (
+            <>
+              <Route path="/produtos" element={<Dashboard />} />
+              <Route path="/produtos/novo" element={<Products />} />
+              <Route path="/ingredientes" element={<Ingredients />} />
+              <Route path="/ingredientes/novo" element={<IngredientForm />} />
+              <Route path="/clientes" element={<Clients />} />
+              <Route path="/clientes/novo" element={<ClientForm />} />
+              <Route path="/pedidos" element={<Orders />} />
+              <Route path="/pedidos/novo" element={<OrderForm />} />
+              <Route path="/pedidos/:id" element={<OrderForm />} />
+              <Route path="/combos" element={<Combos />} />
+              <Route path="/combos/novo" element={<ComboForm />} />
+              <Route path="/combos/:id" element={<ComboForm />} />
+              <Route path="/estoque" element={<Inventory />} />
+              <Route path="/production" element={<Production />} />
+            </>
+          ) : (
+            <Route path="*" element={<Navigate to="/" replace />} />
+          )}
         </Routes>
       </Box>
+
+      {/* Login Dialog */}
+      <Dialog open={loginOpen} onClose={() => setLoginOpen(false)}>
+        <DialogTitle>Acesso Restrito</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            label="Usuário"
+            fullWidth
+            variant="outlined"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <TextField
+            margin="dense"
+            label="Senha"
+            type="password"
+            fullWidth
+            variant="outlined"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setLoginOpen(false)}>Cancelar</Button>
+          <Button onClick={handleLogin} variant="contained">Entrar</Button>
+        </DialogActions>
+      </Dialog>
     </BrowserRouter>
     </ThemeProvider>
   );
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { Box, Typography, Button, Container } from "@mui/material";
 import { Link } from "react-router-dom";
 
-export default function Home() {
+export default function Home({ isLoggedIn, onLoginClick }) {
   return (
     <Container maxWidth="md">
       <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" minHeight="60vh" textAlign="center" gap={3}>
@@ -11,14 +11,23 @@ export default function Home() {
         <Typography variant="h5" color="text.secondary" sx={{ fontWeight: 500, maxWidth: 600 }}>
           Controle total de custos, produção e revenda para o seu negócio de cookies.
         </Typography>
-        <Box display="flex" gap={2} mt={2}>
-          <Button variant="contained" size="large" component={Link} to="/produtos/novo" sx={{ px: 4, py: 1.5 }}>
-            Novo Produto
-          </Button>
-          <Button variant="outlined" size="large" component={Link} to="/produtos" sx={{ px: 4, py: 1.5 }}>
-            Consultar
-          </Button>
-        </Box>
+        
+        {isLoggedIn ? (
+          <Box display="flex" gap={2} mt={2}>
+            <Button variant="contained" size="large" component={Link} to="/produtos/novo" sx={{ px: 4, py: 1.5 }}>
+              Novo Produto
+            </Button>
+            <Button variant="outlined" size="large" component={Link} to="/produtos" sx={{ px: 4, py: 1.5 }}>
+              Consultar
+            </Button>
+          </Box>
+        ) : (
+          <Box mt={2}>
+            <Button variant="contained" size="large" onClick={onLoginClick} sx={{ px: 4, py: 1.5 }}>
+              Fazer Login
+            </Button>
+          </Box>
+        )}
       </Box>
     </Container>
   );
